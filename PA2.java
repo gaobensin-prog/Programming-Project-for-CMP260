372ac967ece38aa1e56914080da237535e962202
Zhi B Gao <gaobensin@gmail.com>
2026-08-31 17:32:50 -0500

    New Understanding unlocked the bracket is what I have to watch for.

--- PA2.java
+++ PA2.java
@@ -18,30 +18,18 @@ public class PA2{
             for (int j = 1; j < 3; j++){
                 if (j == 2){
                     System.out.print("Enter a Number for y:");
-                    Double num = input.nextDouble();
-                    input.nextLine();
-                    nums.add(num);
                 }else{
                     System.out.print("Enter a Number for x:");
-                    Double num = input.nextDouble();
-                    input.nextLine();
-                    nums.add(num);
                 }
+                Double num = input.nextDouble();
+                input.nextLine();
+                nums.add(num);
             }
             values.put(letter, nums);
             nums = new ArrayList<>();
         }
         System.out.println(values);
-            
         
-
-        //System.out.print("Choose a Distance Metric: ");
-        //String distanceMetric = input.nextLine();
-        //input.nextLine();
-        //System.out.print("Choose How Much Decimal Place: ");
-        //Integer decimalPlace = input.nextInt();
-        //input.nextLine();
-        //System.out.print(nums);
 
         
         
